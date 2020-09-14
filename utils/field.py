b79dddb37288fa7125a5e1cfa63b1b66c924f7c0
nick-martin-24 <nsm5102@gmail.com>
2020-09-14 18:14:28 -0400

    added smaller functions for clarity and accounted for thisWeek being 1 week behind

--- utils/field.py
+++ utils/field.py
@@ -4,7 +4,34 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def generate_field_html(field_url, output_directory, filename, ftp_directory):
+def generate_field_html(output_directory, filename, ftp_directory):
+    field_url = get_current_field_url()
+    player_names = get_current_field(field_url)
+    owgr = get_top_60_in_current_field(player_names)
+
+    # define tiers
+    a = owgr[0:10]
+    b = owgr[10:25]
+    c = owgr[25:40]
+    d = owgr[40:]
+    write_field_html(filename, a, b, c, d)
+    gpftp.upload_file_to_ftp(output_directory, 'field.html', ftp_directory)
+
+
+def get_current_field_url():
+    schedule = 'https://statdata.pgatour.com/r/current/schedule-v2.json'
+    schedule_json = requests.get(schedule).json()
+    week = str(int(schedule_json['thisWeek']['weekNumber']) + 1)
+    year = schedule_json['years'][1]
+    tour = year['tours'][0]
+    current_tournament = next(item for item in tour['trns'] if item['date']['weekNumber'] == week
+                              and item['primaryEvent'] == 'Y')
+    tournament_code = current_tournament['permNum']
+    return 'https://statdata.pgatour.com/r/{}/field.json'.format(tournament_code)
+
+
+def get_current_field(field_url):
+    # set player names from field of current tournament
     f = requests.get(field_url)
     parsed_json = f.json()
     players = parsed_json['Tournament']['Players']
@@ -12,8 +39,13 @@ def generate_field_html(field_url, output_directory, filename, ftp_directory):
     for item in players:
         name = item['PlayerName'].split(', ')
         player_names.append(' '.join((name[1], name[0])))
+    return player_names
 
-    url = 'http://www.owgr.com/ranking?pageNo=1&pageSize=300$country=All'
+
+def get_top_60_in_current_field(player_names):
+    # get players from owgr and set the top 60 who are in the field of current tournament
+    # url = 'http://www.owgr.com/ranking?pageNo=1&pageSize=300$country=All'
+    url = 'http://www.owgr.com/ranking'
     h = urllib.request.urlopen(url)
     html = h.read()
     soup = BeautifulSoup(html, 'html.parser')
@@ -28,15 +60,14 @@ def generate_field_html(field_url, output_directory, filename, ftp_directory):
             name = 'Rafa Cabrera Bello'
         if name == 'Peter Uihlein':
             continue
-        if name in player_names and count < 60:
+        if name in player_names and count < 60 and name != 'Scottie Scheffler':
             owgr.append(name)
             count += 1
+    return owgr
 
-    a = owgr[0:10]
-    b = owgr[10:25]
-    c = owgr[25:40]
-    d = owgr[40:]
 
+def write_field_html(filename, a, b, c, d):
+    # write html file
     f = open(filename, 'w')
     header = '''<html>
     <head>
@@ -50,10 +81,10 @@ def generate_field_html(field_url, output_directory, filename, ftp_directory):
 
     body_head = '''
     <body>
-    
+
     <form id="frm1" action = "team_creation.php" method="post">
         <br><br>Your Name: <input type="text" name="user"><br><br><br>
-        
+
         <table style="width:80%">
     '''
     table_header = '''
@@ -69,10 +100,10 @@ def generate_field_html(field_url, output_directory, filename, ftp_directory):
     for i in range(0, 10):
         one_to_ten += '''
             <tr>
-                <td>input type="checkbox" name="a[]" value"''' + a[i] + '''">''' + a[i] + '''</td>
-                <td>input type="checkbox" name="b[]" value"''' + b[i] + '''">''' + b[i] + '''</td>
-                <td>input type="checkbox" name="c[]" value"''' + c[i] + '''">''' + c[i] + '''</td>
-                <td>input type="checkbox" name="d[]" value"''' + d[i] + '''">''' + d[i] + '''</td>
+                <td><input type="checkbox" name="a[]" value"''' + a[i] + '''">''' + a[i] + '''</td>
+                <td><input type="checkbox" name="b[]" value"''' + b[i] + '''">''' + b[i] + '''</td>
+                <td><input type="checkbox" name="c[]" value"''' + c[i] + '''">''' + c[i] + '''</td>
+                <td><input type="checkbox" name="d[]" value"''' + d[i] + '''">''' + d[i] + '''</td>
             </tr>
     '''
 
@@ -81,9 +112,9 @@ def generate_field_html(field_url, output_directory, filename, ftp_directory):
         eleven_to_fifteen += '''
             <tr>
                 <td> </td>
-                <td>input type="checkbox" name="b[]" value"''' + b[i] + '''">''' + b[i] + '''</td>
-                <td>input type="checkbox" name="c[]" value"''' + c[i] + '''">''' + c[i] + '''</td>
-                <td>input type="checkbox" name="d[]" value"''' + d[i] + '''">''' + d[i] + '''</td>
+                <td><input type="checkbox" name="b[]" value"''' + b[i] + '''">''' + b[i] + '''</td>
+                <td><input type="checkbox" name="c[]" value"''' + c[i] + '''">''' + c[i] + '''</td>
+                <td><input type="checkbox" name="d[]" value"''' + d[i] + '''">''' + d[i] + '''</td>
             </tr>
     '''
 
@@ -94,7 +125,7 @@ def generate_field_html(field_url, output_directory, filename, ftp_directory):
                 <td> </td>
                 <td> </td>
                 <td> </td>
-                <td>input type="checkbox" name="d[]" value"''' + d[i] + '''">''' + d[i] + '''</td>
+                <td><input type="checkbox" name="d[]" value"''' + d[i] + '''">''' + d[i] + '''</td>
             </tr>
     '''
 
@@ -113,9 +144,9 @@ def generate_field_html(field_url, output_directory, filename, ftp_directory):
     f.write(footer)
     f.close()
 
-    gpftp.upload_file_to_ftp(output_directory, 'field.html', ftp_directory)
 
-def generate_php_file(output_directory, filename, ftp_directory):
+def generate_php_file(output_directory, ftp_directory):
+    filename = '{}/team_creation.php'.format(output_directory)
     f = open(filename, 'w')
     header = '''<html>
     <body>
@@ -126,18 +157,18 @@ def generate_php_file(output_directory, filename, ftp_directory):
     $a = count($_POST['a']);
     $b = count($_POST['b']);
     $c = count($_POST['c']);
-    $d = count($+POST['d']);
+    $d = count($_POST['d']);
     
     if ($a != 2) {
-        echo "Incorrect number of golfers (\\"" . $a . "\\") in Group A. Must select 2 players. <br><a href=\\"#\\" onclick=\\"history.back();\\">Please try again</a>:
-    } elseif ($b !3) {
-        echo "Incorrect number of golfers (\\"" . $b . "\\") in Group B. Must select 3 players. <br><a href=\\"#\\" onclick=\\"history.back();\\">Please try again</a>:
+        echo "Incorrect number of golfers (\\"" . $a . "\\") in Group A. Must select 2 players. <br><a href=\\"#\\" onclick=\\"history.back();\\">Please try again</a>":
+    } elseif ($b != 3) {
+        echo "Incorrect number of golfers (\\"" . $b . "\\") in Group B. Must select 3 players. <br><a href=\\"#\\" onclick=\\"history.back();\\">Please try again</a>":
     } elseif ($c != 2) {
-        echo "Incorrect number of golfers (\\"" . $c . "\\") in Group C. Must select 2 players. <br><a href=\\"#\\" onclick=\\"history.back();\\">Please try again</a>:
+        echo "Incorrect number of golfers (\\"" . $c . "\\") in Group C. Must select 2 players. <br><a href=\\"#\\" onclick=\\"history.back();\\">Please try again</a>":
     } elseif ($d !2) {
-        echo "Incorrect number of golfers (\\"" . $d . "\\") in Group D. Must select 2 players. <br><a href=\\"#\\" onclick=\\"history.back();\\">Please try again</a>:
+        echo "Incorrect number of golfers (\\"" . $d . "\\") in Group D. Must select 2 players. <br><a href=\\"#\\" onclick=\\"history.back();\\">Please try again</a>":
     } elseif (!isset($_POST['user']) || trim($_POST['user']) == '') {
-        echo "Team name was left blank. <br><a href=\\"#\\" onclick=\\"history.back();\\"Please try again</a>";
+        echo "Team name was left blank. <br><a href=\\"#\\" onclick=\\"history.back();\\">Please try again</a>";
     } elseif (!isset($_POST['tiebreaker']) || trim($_POST['tiebreaker']) == '') {
         echo "Tiebreaker was left blank. <br><a href=\\"#\\" onclick=\\"history.back();\\">Please try again</a>";
     } else {
