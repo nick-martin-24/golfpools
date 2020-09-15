ef34af5c7d4090d3c0dfb1eefb2f9a2b0beaa34d
nick-martin-24 <nsm5102@gmail.com>
2020-09-15 18:12:37 -0400

    added cron.zsh

---
+++ scripts/cron.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+#~/.virtualenvs/golfpools/bin/python ~/projects/python/golfpools/src/main.py
+if ! pgrep -lf python | grep -q main.py; then
+    ~/.virtualenvs/golfpools/bin/python ~/projects/python/golfpools/src/main.py
+fi
